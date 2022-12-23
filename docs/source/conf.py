43696f3af0a442726eebdeb797cb1aabbd507572
Adam WECKER <114078299+AL1061703@users.noreply.github.com>
2022-12-23 14:33:42 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'DataBench'
+copyright = '2023, TEPDK'
+author = 'Adam Wecker'
 
 release = '0.1'
 version = '0.1.0'
